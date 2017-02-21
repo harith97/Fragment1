f45150ef58339ed52b300edfe1fa5c63639cb5b6
harith97 <harithaperera@gmail.com>
2017-02-21 12:46:42 +0530

    Initial commit

---
+++ app/src/main/java/com/example/harithaperera/fragment1/FragmentClass.java
@@ -0,0 +1,21 @@
+package com.example.harithaperera.fragment1;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by harithaperera on 2/17/17.
+ */
+public class FragmentClass extends Fragment{//STATIC WAY OF FRAGMNERT
+
+
+    @Nullable
+    @Override//linking XML
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragmentlayout,container,false);
+    }
+}
---
+++ app/src/main/java/com/example/harithaperera/fragment1/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.harithaperera.fragment1;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
